ac51d774b71e1bf7202fe2f1b0299ece7e2a0dff
Ayush Raj <rnikhilaj06@gmail.com>
2025-08-16 12:39:12 +0530

    Hello world

--- 1000_1200_difficulty/hello_world.c
+++ 1000_1200_difficulty/hello_world.c
@@ -5,3 +5,11 @@ int main() {
     return 0;
 }
 
+/*
+steps to upload codes on github
+1. git status (check git is initialized or not)
+2. git init (to initialize a new git repository)
+3. git add . (to stage all changes)
+4. git commit -m "your commit message" (to commit changes)
+5. git push (to push changes to remote repository)
+*/
